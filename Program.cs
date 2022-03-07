625dfe15ddd4fdac29a3037e5005592e36180462
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2022-03-07 15:38:16 +0200

    FEAT: Add template for group service

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using AlumniNetworkAPI.Services;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddScoped(typeof(IGroupService), typeof(GroupService));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
---
+++ Services/Group/GroupService.cs
@@ -0,0 +1,32 @@
+﻿using AlumniNetworkAPI.Models.Domain;
+
+namespace AlumniNetworkAPI.Services
+{
+    public class GroupService : IGroupService
+    {
+        public Task<Group> AddGroupAsync(Group group, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Group>> GetAllGroupsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Group> GetSpecificGroupAsync(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GroupExists(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task JoinGroupAsync(int groupId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
