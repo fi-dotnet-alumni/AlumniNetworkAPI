402e35e8c6207d190c1d17426d916ee1174df34d
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2022-03-07 14:14:08 +0200

    FEAT: Add domain model and DTOs for Group

---
+++ Models/DTO/Group/GroupEditDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlumniNetworkAPI.Models.DTO.Group
+{
+    public class GroupEditDTO
+    {
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name can't be more than 50 characters long.")]
+        public string? Name { get; set; }
+        [Required]
+        [MaxLength(150, ErrorMessage = "Description can't be more than 150 characters long.")]
+        public string? Description { get; set; }
+        [Required]
+        public bool isPrivate { get; set; }
+    }
+}
---
+++ Models/DTO/Group/GroupReadDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace AlumniNetworkAPI.Models.DTO.Group
+{
+    public class GroupReadDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool isPrivate { get; set; }
+        List<int>? Users { get; set; }
+        List<int>? Posts { get; set; }
+    }
+}
