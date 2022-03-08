b62e2af143eb34b1bb45ffb84a01942a1048a227
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2022-03-08 13:53:45 +0200

    FEAT: Add initial domain models
    Co-authored-by: mikkoryynanen <10918788+mikkoryynanen@users.noreply.github.com>

---
+++ Models/Domain/Post.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlumniNetworkAPI.Models.Domain
+{
+	public class Post
+	{
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50, ErrorMessage = "Title can't be more than 50 characters long.")]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+        public DateTime Timestamp { get; set; }
+        
+        public int SenderId { get; set; }
+        public User Sender { get; set; }
+        
+        public int TargetUserId { get; set; }
+        public User TargetUser { get; set; }
+
+        public int TargetGroupId { get; set; }
+        public Group TargetGroup { get; set; }
+
+        public int TargetTopicId { get; set; }
+        public Topic TargeTopic { get; set; }
+        
+    }
+}
+
--- Models/Domain/Topic.cs
+++ Models/Domain/Topic.cs
@@ -1,19 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace AlumniNetworkAPI.Models.Domain
 {
-    public class Group
-    {
+	public class Topic
+	{
         [Key]
         public int Id { get; set; }
+
         [Required]
         [MaxLength(50, ErrorMessage = "Name can't be more than 50 characters long.")]
         public string Name { get; set; }
-        [MaxLength(150, ErrorMessage = "Description can't be more than 150 characters long.")]
+        
+        [MaxLength(200, ErrorMessage = "Description can't be more than 200 characters long.")]
         public string Description { get; set; }
-        [Required]
-        public bool isPrivate { get; set; }
+
         public ICollection<User> Users { get; set; }
         public ICollection<Post> Posts { get; set; }
     }
 }
+
