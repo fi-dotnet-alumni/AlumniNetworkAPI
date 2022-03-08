cead2150dbc7c7fc2397bfb1e0467831bc4979f8
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2022-03-08 16:42:53 +0200

    FEAT: Initial implementations for group services

--- Services/Group/IGroupService.cs
+++ Services/Group/IGroupService.cs
@@ -4,10 +4,11 @@ namespace AlumniNetworkAPI.Services
 {
     public interface IGroupService
     {
-        public Task<IEnumerable<Group>> GetAllGroupsAsync();
+        public Task<IEnumerable<Group>> GetAllGroupsAsync(int userId);
         public Task<Group> GetSpecificGroupAsync(int groupId);
-        public Task<Group> AddGroupAsync(Group group, int userId);
-        public Task JoinGroupAsync(int groupId, int userId);
+        public Task<Group> AddGroupAsync(Group group);
+        public Task JoinGroupAsync(Group group, int userId);
         public bool GroupExists(int groupId);
+        public Task<bool> UserHasGroupAccess(Group group, int userId);
     }
 }
