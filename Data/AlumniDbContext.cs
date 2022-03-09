d6fcfba172f6c9279122e112d7ce545f51e31e9c
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2022-03-09 15:26:10 +0200

    CHORE: Add posts to db context

--- Data/AlumniDbContext.cs
+++ Data/AlumniDbContext.cs
@@ -8,6 +8,7 @@ namespace AlumniNetworkAPI.Data
 	{
         public DbSet<User> Users { get; set; }
         public DbSet<Group> Groups { get; set; }
+        public DbSet<Post> Posts { get; set; }
 
         public AlumniDbContext(DbContextOptions options) : base(options)
 		{
