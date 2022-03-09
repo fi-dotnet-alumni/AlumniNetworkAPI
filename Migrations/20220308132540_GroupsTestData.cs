1e3d92bafb06c1b0ed9c0eb5e0a97604d52a8f44
Mikko <mikko.ryynanen2@gmail.com>
2022-03-09 10:01:16 +0200

    CHORE: Added missing migration file

---
+++ Migrations/20220308132540_GroupsTestData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlumniNetworkAPI.Migrations
+{
+    public partial class GroupsTestData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "GroupUser",
+                columns: new[] { "GroupsId", "UsersId" },
+                values: new object[] { 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "GroupUser",
+                columns: new[] { "GroupsId", "UsersId" },
+                values: new object[] { 3, 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GroupUser",
+                keyColumns: new[] { "GroupsId", "UsersId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupUser",
+                keyColumns: new[] { "GroupsId", "UsersId" },
+                keyValues: new object[] { 3, 1 });
+        }
+    }
+}
